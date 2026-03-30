ac41b4ae1fc30b7d0872a58b1e12a7849c682db2
xXmartinzpXx <zepepue007@gmail.com>
2026-03-30 16:21:39 -0300

    lab/insert_tabla_pedidos

---
+++ ejercicio1_topicos_de_datos.sql
@@ -0,0 +1,8 @@
+SQL> select * from detallespedidos;
+
+ DETALLEID   PEDIDOID PRODUCTOID   CANTIDAD
+---------- ---------- ---------- ----------
+         1        101          1          2
+         2        101          2          5
+         3        101          2          8
+         4        101          1          3
